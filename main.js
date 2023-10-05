4591d942691b3a34298e713215bb6b61aa1d19e2
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-10-05 08:35:25 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,8 +3,44 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const init = ()=>{
+
+  const hideModal = document.getElementById('modal');
+  const likeGlyphs = document.getElementsByClassName('like-glyph')
+  const errorMessage = document.getElementById('modal-message')
 
 
+  function toggleHeartColor(likeGlyph) {
+    if (likeGlyph.classList.contains('activated-heart')) {
+      likeGlyph.textContent = EMPTY_HEART; // Change back to empty heart
+      likeGlyph.classList.remove('activated-heart'); // Remove the class
+    } else {
+      likeGlyph.textContent = FULL_HEART; // Change to full heart
+      likeGlyph.classList.add('activated-heart'); // Add the class to make it red
+    }
+  }
+
+  Array.from(likeGlyphs).forEach(likeGlyph => {
+    likeGlyph.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          // Simulate a successful server response
+          toggleHeartColor(likeGlyph);
+        })
+        .catch(error => {
+          if (hideModal) {
+            hideModal.classList.remove('hidden');
+            errorMessage.textContent = error;
+          }
+          setTimeout(() => {
+            hideModal.classList.add('hidden');
+          }, 3000); // Hide the modal after 3 seconds
+        });
+    });
+  })
+};
+  
+document.addEventListener('DOMContentLoaded',init)
 
 
 //------------------------------------------------------------------------------
